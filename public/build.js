950c2a27ccd7df8e56a6ee00275c70347231890d
guAnsunyata <guansunyata@gmail.com>
2015-12-20 17:45:41 +0800

    demo 0.1 without server side
    done : web storage, answer, submit
    undone : no webStorage & xls proc &server side

--- public/build.js
+++ public/build.js
@@ -1,10 +1,24 @@
 
 
 $(document).ready(function(){
+	//temporay global object before factory object is done
 	$nav = $('.test-nav-l #nav-body');
 	$title = $('.test-body-title');
 	$video = $('.test-body-video video');
 	$des = $('.test-body-des');
+	$answer = $('.answer');
+	$test_body_nav = $('.test-body-nav');
+	$finish_btn_html = "<button class='btn btn-primary submit-btn' style='font-size: 20px; padding: 5px 12px 5px 12px;'>完成測驗</button>";
+	$event_finish_btn = function event_finish_btn (){
+		//submit-btn
+		$('.submit-btn').click(function(){
+			if(window.sessionStorage || window.localStorage){
+				location.href = './result.html#A';
+			}else{
+				
+			}
+		});
+	};
 	var data = [{
 		id: 0,
 		title: '第一題',
@@ -23,103 +37,42 @@ $(document).ready(function(){
 		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
 		videoURL: 'test-media/3.mp4',
 		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
-	},{
-		id: 2,
-		title: '第三題',
-		des: '他能讓你願意配合他嗎？比如他想看電影，他的說法或態度能讓你也願意看電影。',
-		videoURL: 'test-media/3.mp4',
-		answer: '',
 	}];
+	//storage
+	var finish_count = 0;
+	if(window.sessionStorage || window.localStorage){
+		console.log(JSON.parse(sessionStorage.getItem('psycho-Answer-storage-A')));
+		if(sessionStorage.getItem('psycho-Answer-storage-A')){
+			//若已經有記錄，使用記錄作為資料。
+			data = JSON.parse(sessionStorage.getItem('psycho-Answer-storage-A'));
+			finish_count = sessionStorage.getItem('psycho-Answer-storage-A-finish_count');
+		}
+	}else{
+		alert('警告：您的瀏覽器不支援永久儲存功能，請勿重新整理或關閉瀏覽器。(可執行資料夾中的chrome安裝檔，使用chrome可獲得此功能)');
+	}
 	
-	var switcher = new test_switcher(data);
+	var switcher = new test_switcher(data, finish_count);
 	function append_nav(data){
+		var done_span = "<span class='check done'></span>";
+		var undone_span = "<span class='check undone'></span>"
 		data.forEach(function(e){
 			//$nav.append("<span class='nav-cell' id='nav"+e.id+"'><span class='check undone'>未填</span>("+e.id+")"+e.title+"</span><br/>");
-			$nav.append("<a id='nav"+e.id+"' class='list-group-item nav-cell'><span class='check undone'>未填</span>("+e.id+")"+e.title+"</a>");
+			if(e.answer){
+				$nav.append("<a id='nav"+e.id+"' class='list-group-item nav-cell'>"+done_span+"("+e.id+")"+e.title+"</a>");
+			}else{
+				$nav.append("<a id='nav"+e.id+"' class='list-group-item nav-cell'>"+undone_span+"("+e.id+")"+e.title+"</a>");
+			}
 			$('#nav'+e.id).click(function(){
 				switcher._switch(e.id);
 			});
 		});
 	}
 	append_nav(data);
+	if(switcher.finish_count >= switcher.data_length){
+		switcher.alert_finish = false;
+		$test_body_nav.append($finish_btn_html);
+		$event_finish_btn();
+	}
 
 	switcher._switch(0);
 	//$video[0].pause();
@@ -136,20 +89,46 @@ $(document).ready(function(){
 		}
 	});
 
+	//next-btn
 	$('.next-btn').click(function(){
 		switcher._switchNext();
 	});
+
+	//answer
+	$answer.click(function(){
+		switcher.setAnswer($(this).attr('id'));
+		$siblings_div = $(this).siblings();
+		//answer-siblings
+		$.each($siblings_div, function(){
+			$(this).children('img').attr('src','images/'+ $(this).attr('id')+'.png');
+		});
+		//answer-onclick
+		$(this).children('img').attr('src','images/'+$(this).attr('id')+'selected.png');
+		console.log('set answer ',$(this).attr('id'));
+	});
+
 });
 
-function test_switcher(data){ //耦合於data
+function test_switcher(data, finish_count){ //耦合於data
 	var switcher = {
 		current_test: 0,
-		finish_count: 0,
+		finish_count: finish_count,
+		alert_finish: true,
 		data_length: data.length,
 		testSwitch: function(id){
-			this.current_test = id;
-			$('#nav'+id).addClass('nav-avtive').siblings().removeClass('nav-avtive');
 			console.log('test switched:', id);
+			this.current_test = id;
+			//nav 更新
+			$('#nav'+id).addClass('nav-avtive').siblings().removeClass('nav-avtive');
+			//answer 更新
+			console.log('get getAnswer: ',this.getAnswer(id));
+			var current_answer = this.getAnswer(id);
+			$.each($answer, function(){
+				$(this).children('img').attr('src','images/'+ $(this).attr('id')+'.png');
+			});
+			if(current_answer != ''){
+				$('.answer#'+current_answer).children('img').attr('src','images/'+current_answer+'selected.png');
+			}
 		},
 		appendTitle: function(id, content){
 			var html = "<h3>("+id+") "+content+"</h3>"
@@ -177,12 +156,28 @@ function test_switcher(data){ //耦合於data
 		getCurrent: function getCurrent(){
 			return this.current_test
 		},
-		setAnswer: function setAnswer(id,ans){
-			data[id].answer = ans;
-			finish_count++;
-			if(finish_count >= data_length){
-				$('test-bodt-nav').append('<button class="btn-primary next-btn" style="font-size: 20px; padding: 5px 12px 5px 12px;">完成測驗</button>');
+		setAnswer: function setAnswer(ans){
+			data[this.current_test].answer = ans;
+			var temp_count = 0;
+			data.forEach(function(d){
+				if(d.answer != '') temp_count++;
+			});
+			this.finish_count = temp_count;
+			sessionStorage.setItem('psycho-Answer-storage-A', JSON.stringify(data));
+			sessionStorage.setItem('psycho-Answer-storage-A-finish_count', this.finish_count);
+			console.log(this.data_length);
+			if(this.finish_count >= this.data_length && this.alert_finish){
+				if(this.alert_finish){
+					window.setTimeout(function(){alert('您已完成測驗！請點選頁面下方的按鈕將答案送出。')},10);
+					this.alert_finish = false;
+					$test_body_nav.append($finish_btn_html);
+					$event_finish_btn();
+				}
 			}
+			$('#nav'+this.current_test+' span').removeClass('undone').addClass('done');
+		},
+		getAnswer: function getAnswer(id){
+			return data[id].answer
 		},
 		printResult: function printResult(){
 			return this.data.answer
